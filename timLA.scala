6d79c4c2b655694ba0b066ae45f620116fc99639
bindusri0702 <60285438+bindusri0702@users.noreply.github.com>
2021-05-23 18:30:35 +0530

    Delete timLA.scala

--- timLA.scala
+++
@@ -1,4 +0,0 @@
-val loanTermMonthsRdd = kivaSchemaRdd.map{ case(id,fa,la,act,sec,use,cncd,cntr,curr,pt,dt,ft,tim,bg,rint,ds) =>
-(la,tim)}
-
-loanTermMonthsRdd.toDF("loan amount","term in months").show()
