94f8fbf3054311cec8571774e29b6e7a9e283731
bindusri0702 <60285438+bindusri0702@users.noreply.github.com>
2021-05-23 18:29:46 +0530

    Delete dateLA.scala

--- dateLA.scala
+++
@@ -1,4 +0,0 @@
-val dateLaRdd = kivaSchemaRdd.map{ case(id,fa,la,act,sec,use,cncd,cntr,curr,pt,dt,ft,tim,bg,rint,ds) =>
-(ds,la)}
-val highestLa = dateLaRdd.sortBy(_._2,ascending = false)
-highestLa.take(5)
