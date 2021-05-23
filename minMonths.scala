88de48d5248756b86d45d0e53182ad9ede11d761
bindusri0702 <60285438+bindusri0702@users.noreply.github.com>
2021-05-23 18:30:27 +0530

    Delete minMonths.scala

--- minMonths.scala
+++
@@ -1,5 +0,0 @@
-
-val minMonthsRdd = kivaSchemaRdd.map{ case(id,fa,la,act,sec,cncd,cntr,curr,pt,dt,ft,tim,lenCnt,bg,rint,ds) =>
-	(cntr,tim)}.reduceByKey(_+_).sortBy(_._2)
-
-minMonthsRdd.toDF("Country","Term in Months").show()
